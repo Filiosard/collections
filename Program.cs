7b45a6258b271464e6d985c67f805f2b6fd3f8e2
Filiosard <igorich457@gmail.com>
2026-04-25 16:25:13 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> list = new HashSet<string>();
+            list.Add("Umamu");
+            list.Add("Cortisol");
+            list.Add("FutanariSasuke");
+            list.Add("Umamu");
+            list.Add("Umamu");
+            list.Add("Umamu");
+            list.Add("Umamu");
+            list.Add("Umamu");
+            list.Add("Umamu");
+
+            foreach (var name in list)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+};
+
