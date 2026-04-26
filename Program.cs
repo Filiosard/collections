762925ad8d24fea683506c7f2f8f30bfc59889f4
Nikita Buinovskyi <nikita.buinovskyi@student.tuke.sk>
2026-04-26 10:32:01 +0200

    el condor pasa added to set

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@
             list.Add("Umamu");
             list.Add("Cortisol");
             list.Add("FutanariSasuke");
+            list.Add("El Condor Pasa");
             foreach (var name in list)
             {
                 Console.WriteLine(name);
